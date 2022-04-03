f78ee0f7397264f71e71271aeb0334acb5efb40e
Ferrin <fthreatt19@gmail.com>
2022-04-03 09:48:02 -0400

    updated nav menu

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
 import React from "react";
 import NavMenu from "./components/NavMenu";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./pages/home";
+import About from "./pages/about";
+import Projects from "./pages/projects";
+import Contact from "./pages/contact";
 
 export default function App() {
   return (
     <>
       <Router>
         <NavMenu />
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/projects">
+            <Projects />
+          </Route>
+          <Route path="/contact">
+            <Contact />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
       </Router>
     </>
   );
-}
+};
